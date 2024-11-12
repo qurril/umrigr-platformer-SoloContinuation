f0f0f5a3ee8fabbffc293dbf2db97397346d763f
dsirovat123 <damjan.sirovatka@fer.hr>
2024-11-12 16:58:34 +0100

    structure update

---
+++ Assets/Scripts/GameItself/GameManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+
+public class GameManager : NetworkBehaviour
+{
+    private static GameManager _instance;
+    public override void Spawned()
+    {
+
+    }
+}
---
+++ Assets/Scripts/MainMenu/SessionView.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SessionView : Singleton<SessionView>
+{
+    public void UpdateSessionList() {
+    
+    }
+}
